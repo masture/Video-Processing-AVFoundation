ab57e26fa39ab6c75d32ed7c333a32d445681d8d
Pankaj Kulkarni <kulkarnipanku@gmail.com>
2019-01-15 14:45:22 +0530

    Playing save video twice. One in top-right corner and second shrunk even more and rotated y 30 degrees. At present the audio is not playing.

--- PictureInPicture/ViewController.swift
+++ PictureInPicture/ViewController.swift
@@ -7,14 +7,221 @@
 //
 
 import UIKit
+import AVFoundation
+import MobileCoreServices
+import Photos
 
 class ViewController: UIViewController {
+    
+    var isFirstAssetLoaded = false
+    var isSecondAssetLoaded = false
+    var firstAsset: AVURLAsset!
+    var secondAsset: AVURLAsset!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let videoURL = Bundle.main.url(forResource: "IMG_1354", withExtension: "m4v")!
+        
+        firstAsset = AVURLAsset(url: videoURL)
+        secondAsset = AVURLAsset(url: videoURL)
+        
+        let assetKeysToLoad = ["tracks", "duration", "composable"]
+        
+        firstAsset.loadValuesAsynchronously(forKeys: assetKeysToLoad) {
+            self.isFirstAssetLoaded = true
+        }
+        
+        secondAsset.loadValuesAsynchronously(forKeys: assetKeysToLoad) {
+            self.isSecondAssetLoaded = true
+        }
     }
 
+    @IBAction func pictureInPicturePressed(_ sender: UIButton) {
+        
+        
+        guard isFirstAssetLoaded else {
+            print("First asset is not yet loaded")
+            return
+        }
+        
+        guard isSecondAssetLoaded else {
+            print("Second asset is not yet loaded")
+            return
+        }
+        
+        let mixComposition = AVMutableComposition()
+        let audioMix = AVMutableAudioMix()
+        
+        // Create first compostion track from first asset
+        let firstCompositionTrack = mixComposition.addMutableTrack(withMediaType: AVMediaType.video, preferredTrackID: kCMPersistentTrackID_Invalid)
+        
+        let timeRange = CMTimeRange(start: CMTime.zero, duration: firstAsset.duration)
+        let firstVideoTrack = firstAsset.tracks(withMediaType: AVMediaType.video).first!
+        
+        do {
+            try firstCompositionTrack?.insertTimeRange(timeRange, of: firstVideoTrack, at: CMTime.zero)
+        } catch {
+            fatalError("Error inserting time range into first compostion track! :\(error)")
+        }
+        
+        // Add the audio of the first track to the video
+        let firstAudioCompostionTrack = mixComposition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)
 
+        let firstAudioTrack = firstAsset.tracks(withMediaType: .audio).first!
+
+        do {
+            try firstAudioCompostionTrack?.insertTimeRange(timeRange, of: firstAudioTrack, at: CMTime.zero)
+        } catch {
+            fatalError("Error inserting audio asset into first audio composition track - \(error)")
+        }
+        
+        // Set audio input parameter
+        let audioMixInputParameter = AVMutableAudioMixInputParameters(track: firstAudioCompostionTrack)
+        audioMixInputParameter.setVolume(1.0, at: CMTime.zero)
+        audioMix.inputParameters = [audioMixInputParameter]
+        
+        // Create second compostion track from second asset
+        let secondCompostionTrack = mixComposition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid)
+        
+        let secondVideoTrack = secondAsset.tracks(withMediaType: .video).first!
+        
+        do {
+            try secondCompostionTrack?.insertTimeRange(timeRange, of: secondVideoTrack, at: CMTime.zero)
+        } catch {
+            fatalError("Error inserting time range into second compostion track! :\(error)")
+        }
+        
+        // Create one video composition instruction. It will have multiple video composition layer instructions
+        // We can have as many video ccompostion instructions as we need.
+        let videoCompostionInstruction = AVMutableVideoCompositionInstruction()
+        
+        videoCompostionInstruction.timeRange = timeRange
+        
+        let finalVideoSize = firstVideoTrack.naturalSize
+        
+        // Create vide composition layer instruction for the first video track
+        let firstLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: firstVideoTrack)
+        
+        // Half this video in height and width and set it in the bottom right corner!
+//        let scale = CGAffineTransform(scaleX: 0.5, y: 0.5)
+//        let move = CGAffineTransform(translationX: finalVideoSize.width/2.0, y: finalVideoSize.height/2.0)
+//
+//        firstLayerInstruction.setTransform(scale.concatenating(move), at: CMTime.zero)
+        
+        // Create a video of size 0.3 x 0.3, move it to center and rotate by 30 degrees
+        let scale = CGAffineTransform(scaleX: 0.3, y: 0.3)
+        let move = CGAffineTransform(translationX: finalVideoSize.width/2.0, y: finalVideoSize.height/2.0)
+        let rotate = CGAffineTransform(rotationAngle: CGFloat.pi/6.0)
+        
+        firstLayerInstruction.setTransform(rotate.concatenating(scale).concatenating(move), at: CMTime.zero)
+        
+        
+        // Create second layer compostion instructions for the second video track
+        
+        let secondLayerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: secondVideoTrack)
+//        let scaleSecondVideo = CGAffineTransform(scaleX: 1, y: 1)
+        // Scale down the second vidoe by 50% both width and height wise
+        let scaleSecondVideo = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        let moveSecondVideo = CGAffineTransform(translationX: 0, y: 0)
+        
+        secondLayerInstruction.setTransform(scaleSecondVideo.concatenating(moveSecondVideo), at: CMTime.zero)
+        
+        
+        // Add both video compostion layer instructions to main video compostion instruction
+        videoCompostionInstruction.layerInstructions = [firstLayerInstruction, secondLayerInstruction]
+        
+        // Create video compostion. It will have one or more video compoistion instructions.
+        // The instructions themselves will have one or more layer instructions
+        
+        let videoComposition = AVMutableVideoComposition()
+        videoComposition.instructions = [videoCompostionInstruction]
+        videoComposition.frameDuration = CMTimeMake(value: 1, timescale: 30)
+        videoComposition.renderSize = CGSize(width: firstVideoTrack.naturalSize.width, height: firstVideoTrack.naturalSize.height)
+        
+        // For debuggging purpose
+        videoComposition.isValid(for: nil, timeRange: CMTimeRangeMake(start: CMTime.zero, duration: CMTime.positiveInfinity), validationDelegate: self)
+        
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .short
+        let date = dateFormatter.string(from: Date())
+        let url = documentDirectory.appendingPathComponent("mergeVideo-\(date).mov")
+        
+        // 5 - Create Exporter
+        guard let exporter = AVAssetExportSession(asset: mixComposition, presetName: AVAssetExportPresetHighestQuality) else { return }
+        
+        exporter.outputURL = url
+        exporter.outputFileType = AVFileType.mov
+        exporter.shouldOptimizeForNetworkUse = true
+        exporter.videoComposition = videoComposition
+        exporter.audioMix = audioMix
+        
+        print("Starting export asynchronously...")
+        // 6 - Perform the Export
+        exporter.exportAsynchronously() {
+            print("Finished export.. moving file to photo library...")
+            DispatchQueue.main.async {
+                self.exportDidFinish(exporter)
+            }
+        }
+        
+        
+    }
+
+    
+    
+    func exportDidFinish(_ session: AVAssetExportSession) {
+        
+        guard session.status == AVAssetExportSession.Status.completed,
+            let outputURL = session.outputURL else { return }
+        
+        let saveVideoToPhotos = {
+            PHPhotoLibrary.shared().performChanges({ PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: outputURL) }) { saved, error in
+                let success = saved && (error == nil)
+                let title = success ? "Success" : "Error"
+                let message = success ? "Video saved" : "Failed to save video"
+                
+                let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+        
+        // Ensure permission to access Photo Library
+        if PHPhotoLibrary.authorizationStatus() != .authorized {
+            PHPhotoLibrary.requestAuthorization({ status in
+                if status == .authorized {
+                    saveVideoToPhotos()
+                }
+            })
+        } else {
+            saveVideoToPhotos()
+        }
+    }
+    
 }
 
+extension ViewController: AVVideoCompositionValidationHandling {
+    
+    func videoComposition(_ videoComposition: AVVideoComposition, shouldContinueValidatingAfterFindingInvalidValueForKey key: String) -> Bool {
+        print("The key, \(key) not found!")
+        return true
+    }
+    
+    func videoComposition(_ videoComposition: AVVideoComposition, shouldContinueValidatingAfterFindingEmptyTimeRange timeRange: CMTimeRange) -> Bool {
+        print("Empty time range found!")
+        return true
+    }
+    
+    func videoComposition(_ videoComposition: AVVideoComposition, shouldContinueValidatingAfterFindingInvalidTimeRangeIn videoCompositionInstruction: AVVideoCompositionInstructionProtocol) -> Bool {
+        print("Invalid time range in video compostion instruction")
+        return true
+    }
+    
+    func videoComposition(_ videoComposition: AVVideoComposition, shouldContinueValidatingAfterFindingInvalidTrackIDIn videoCompositionInstruction: AVVideoCompositionInstructionProtocol, layerInstruction: AVVideoCompositionLayerInstruction, asset: AVAsset) -> Bool {
+        print("Found an invalid track")
+        return true
+    }
+}
